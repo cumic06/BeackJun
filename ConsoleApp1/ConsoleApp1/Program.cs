ed2d88a785bbf600f7d7d9857832375073a3b0f4
bch6224 <a01022105263@gmail.com>
2024-02-11 00:37:36 +0900

    Sort
    정렬

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 90)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    int c = a + b;
+                    a = b;
+                    b = c;
+                }
+                Console.WriteLine(a);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int[] sortArray = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                sortArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(sortArray);
+
+            StringBuilder stringBuilder = new StringBuilder(string.Join("\n", sortArray));
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
