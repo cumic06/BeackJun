ec722377768a956f4fa0a5513780bb2bf8ca5e89
bch6224 <a01022105263@gmail.com>
2024-02-14 01:08:15 +0900

    stringWhile
    도대체 alphanumeric 이건 왜 적혀있던걸까..

--- stringWhile/stringWhile/Program.cs
+++ stringWhile/stringWhile/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace stringWhile
 {
@@ -10,6 +6,29 @@ namespace stringWhile
     {
         static void Main(string[] args)
         {
+            string result = "";
+            int t = int.Parse(Console.ReadLine());
+            if (t > 1001) return;
+            for (int i = 0; i < t; i++)
+            {
+                string[] rs = Console.ReadLine().Split();
+                int r = int.Parse(rs[0]);
+                string s = rs[1];
+
+                if (s.Length > 20) return;
+                if (s.Length <= 0) return;
+
+                for (int l = 0; l < s.Length; l++)
+                {
+                    for (int j = 0; j < r; j++)
+                    {
+                        result += s[l];
+                    }
+                }
+
+                Console.WriteLine(result);
+                result = "";
+            }
         }
     }
 }
