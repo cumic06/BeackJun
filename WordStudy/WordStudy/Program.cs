dc026a942c13c01c179eea5d3f2504622a5f58cb
bch6224 <a01022105263@gmail.com>
2024-02-16 01:57:00 +0900

    WordStudy

---
+++ WordStudy/WordStudy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToUpper();
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
