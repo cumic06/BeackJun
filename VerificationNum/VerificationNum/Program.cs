d1b8f33246f5bc53042ff4f2cf1c27ae6742fa17
bch6224 <a01022105263@gmail.com>
2024-02-16 02:03:39 +0900

    VerificationNum

---
+++ VerificationNum/VerificationNum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VerificationNum
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]) * int.Parse(str[0]);
+            int b = int.Parse(str[1]) * int.Parse(str[1]);
+            int c = int.Parse(str[2]) * int.Parse(str[2]);
+            int d = int.Parse(str[3]) * int.Parse(str[3]);
+            int e = int.Parse(str[4]) * int.Parse(str[4]);
+            int sum = a + b + c + d + e;
+            int result = sum % 10;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
