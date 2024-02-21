b06841c735d62c3495cb22d1dfb6ed9f7fc0805c
bch6224 <a01022105263@gmail.com>
2024-02-22 00:13:02 +0900

    Average

---
+++ Average/Average/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace Average
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] score = Console.ReadLine().Split();
+
+            int[] scoreInt = Array.ConvertAll(score, s => int.Parse(s));
+
+            int highValue = scoreInt.Max();
+
+            double a = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                a += double.Parse(score[i]) / highValue * 100;
+            }
+            double b = a / n;
+            Console.WriteLine(b);
+        }
+    }
+}
