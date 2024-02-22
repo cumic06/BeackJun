5aa13ac02aa716adbf8fe393373d5cc7015767c4
bch6224 <a01022105263@gmail.com>
2024-02-22 17:02:09 +0900

    PrimeNumber

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] pn = Console.ReadLine().Split();
+            int[] pnInt = Array.ConvertAll(pn, s => int.Parse(s));
+
+            for (int i = 0; i < n; i++)
+            {
+                if (pnInt[i] % 2 != 0)
+                {
+
+                }
+                if (pnInt[i] % 3 != 0)
+                {
+
+                }
+                if (pnInt[i] % 5 != 0)
+                {
+
+                }
+                if (pnInt[i] % 7 != 0)
+                {
+
+                }
+            }
+        }
+    }
+}
