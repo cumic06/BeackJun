51a35d1a79206f0d67d41815365346c9b61653ed
bch6224 <a01022105263@gmail.com>
2024-02-24 01:03:05 +0900

    zero

---
+++ Zero/Zero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zero
+{
+    class Program
+    {
+        static void Main()
+        {
+            int k = int.Parse(Console.ReadLine());
+            Stack<int> aStack = new Stack<int>();
+
+            for (int i = 0; i < k; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n.Equals(0))
+                {
+                    aStack.Pop();
+                }
+                else
+                {
+                    aStack.Push(n);
+                }
+            }
+
+            Console.WriteLine(aStack.Sum());
+        }
+    }
+}
