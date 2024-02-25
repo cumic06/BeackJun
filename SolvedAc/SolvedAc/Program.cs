abc5bc240d0143a805a9d9ab190509de7a507d27
bch6224 <a01022105263@gmail.com>
2024-02-26 01:51:28 +0900

    Stack

---
+++ SolvedAc/SolvedAc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SolvedAc
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int cut = CuttingAverage(a);
+            int[] contributor = new int[a];
+            int difficulty = 0;
+            if (a != 0)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    int b = int.Parse(Console.ReadLine());
+                    contributor[i] = b;
+                }
+
+                Array.Sort(contributor);
+
+                for (int i = cut; i < contributor.Length - cut; i++)
+                {
+                    difficulty += contributor[i];
+                }
+
+                difficulty = (int)Math.Round((float)difficulty / (contributor.Length - (cut * 2)), MidpointRounding.AwayFromZero);
+            }
+            Console.WriteLine(difficulty);
+        }
+
+        static int CuttingAverage(int a)
+        {
+            return (int)Math.Round((float)a * 0.15f, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static Stack<int> intStack = new Stack<int>();
+        static StringBuilder sb = new StringBuilder();
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Stack(Console.ReadLine().Split());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void Stack(string[] a)
+        {
+            string command = a[0];
+            int value = 0;
+            if (a.Length > 1)
+            {
+                value = int.Parse(a[1]);
+            }
+            if (command == "push")
+            {
+                intStack.Push(value);
+            }
+            if (command == "top")
+            {
+                if (intStack.Count == 0)
+                {
+                    sb.AppendLine("-1");
+                }
+                else
+                {
+                    sb.AppendLine($"{intStack.Peek()}");
+                }
+            }
+            if (command == "size")
+            {
+                sb.AppendLine($"{intStack.Count}");
+            }
+            if (command == "empty")
+            {
+                if (intStack.Count == 0)
+                {
+                    sb.AppendLine("1");
+                }
+                else
+                {
+                    sb.AppendLine("0");
+                }
+            }
+            if (command == "pop")
+            {
+                if (intStack.Count == 0)
+                {
+                    sb.AppendLine("-1");
+                }
+                if (intStack.Count >= 1)
+                {
+                    sb.AppendLine($"{intStack.Pop()}");
+                }
+            }
+        }
+    }
+}
