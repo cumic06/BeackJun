cf2bcb91774a61aa444aaeb5631fcb695b93f203
bch6224 <a01022105263@gmail.com>
2024-02-27 17:15:15 +0900

    AgeSort

---
+++ AgeSort/AgeSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgeSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            string[] account;
+
+            for (int i = 0; i < n; i++)
+            {
+                account = Console.ReadLine().Split();
+                Array.Sort(account);
+                sb.AppendLine(account[0] + " " + account[1]);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
