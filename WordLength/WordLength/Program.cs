b0ec72d52235bc4997be1571487a7ae7fc2db3b9
bch6224 <a01022105263@gmail.com>
2024-03-02 23:10:33 +0900

    WordLength

---
+++ WordLength/WordLength/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordLength
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Console.ReadLine().Length);
+        }
+    }
+}
