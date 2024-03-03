b456e25ad358a7c4857462b95134822bb98a9e0c
bch6224 <a01022105263@gmail.com>
2024-03-03 23:21:05 +0900

    ChangeCase

---
+++ ChangeCase/ChangeCase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ChangeCase
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = Console.ReadLine();
+            char[] b = a.Select(str => char.IsUpper(str) ? char.ToLower(str) : char.ToUpper(str)).ToArray();
+            Console.WriteLine(b);
+        }
+    }
+}
