522ce14b2f6384a8c3d1f47f1d3b661cdf0f379a
bch6224 <a01022105263@gmail.com>
2024-03-08 01:02:04 +0900

    CreditCalculation

---
+++ JustWrite/JustWrite/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace JustWrite
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                string s = Console.ReadLine().Trim();
+                sb.AppendLine(s);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
