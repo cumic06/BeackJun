b0d0decc01c3f17d437c4556590e74034f92f9ad
bch6224 <changhwan060224@gmail.com>
2024-04-19 23:17:03 +0900

    2024-04-19

---
+++ AdmissionManagement/AdmissionManagement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace AdmissionManagement
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                sb.AppendLine(Console.ReadLine().ToLower());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
