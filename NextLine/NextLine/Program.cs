9ef441b773bee9de3ad7f82475a979bb040a761f
cumic <changhwan060224@gmail.com>
2024-04-20 22:33:05 +0900

    2024-04-20

---
+++ NextLine/NextLine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NextLine
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = b - a;
+            int d = b + c;
+
+            Console.WriteLine(d);
+        }
+    }
+}
