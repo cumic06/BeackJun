d6152febf58e972a1443e4f6de3e828502d94d8e
cumic <changhwan060224@gmail.com>
2024-04-23 00:02:51 +0900

    2024-04-22

---
+++ TopSecret/TopSecret/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace TopSecret
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                string a = Console.ReadLine();
+                if (a == "END") break;
+
+                char[] charA = a.ToCharArray();
+                Array.Reverse(charA);
+
+                foreach (var b in charA)
+                {
+                    sb.Append(b);
+                }
+                sb.AppendLine();
+
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ comparingTwoNumbers/comparingTwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace comparingTwoNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
