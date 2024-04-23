062b2a9b7c02d5ea5f01efe69819f9fed47e6898
cumic <changhwan060224@gmail.com>
2024-04-24 01:40:19 +0900

    2024-04-23

---
+++ Submission/Submission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> student = new List<int>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 28; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                student.Add(a);
+            }
+
+            for (int i = 1; i <= 30; i++)
+            {
+                if (!student.Contains(i))
+                {
+                    sb.AppendLine(i.ToString());
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
