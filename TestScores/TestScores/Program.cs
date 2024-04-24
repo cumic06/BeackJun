0d77d57adc8cc6f7458ae61b8516b1d91996b892
cumic <changhwan060224@gmail.com>
2024-04-24 22:43:01 +0900

    2024-04-24

---
+++ TestScores/TestScores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestScores
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (a >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (a >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (a >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
