62f6d1f7118fead49e6a6f58b823cd39018836e7
cumic <changhwan060224@gmail.com>
2024-05-04 00:11:32 +0900

    2024-05-04

---
+++ AlarmClock/AlarmClock/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace AlarmClock
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] strArray = Console.ReadLine().Split();
+            int h = int.Parse(strArray[0]);
+            int m = int.Parse(strArray[1]);
+
+            m -= 45;
+
+            if (m < 0)
+            {
+                h--;
+                if (h < 0)
+                {
+                    h += 24;
+                }
+
+                m = 60 - Math.Abs(m);
+                sb.Append($"{h} {m}");
+            }
+            else
+            {
+                sb.Append($"{h} {m}");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ leapyear/leapyear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace leapyear
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = Console.ReadLine();
+            int b = int.Parse(a);
+
+            if (b % 4 == 0 && b % 100 != 0 || b % 400 == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
