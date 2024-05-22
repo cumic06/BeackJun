211060a28014a2d4e5115cc41bc8d9db99ec31b4
cumic <changhwan060224@gmail.com>
2024-05-23 00:12:50 +0900

    FibonacciDP
    피보나치 수열로 DP공부중

---
+++ FibonacciDP/FibonacciDP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FibonacciDP
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int fibonacciCount = 0;
+            for (int i = 0; i < a; i++)
+            {
+                fibonacciCount += Fibonacci(a);
+            }
+            Console.WriteLine(fibonacciCount);
+        }
+
+        static int Fibonacci(int a)
+        {
+            return a;
+        }
+        //static int FibonacciDP(int a)
+        //{
+
+        //}
+    }
+}
