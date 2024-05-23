179950acb5c2c292c3da2ca9d950e5f230d2afc1
cumic <changhwan060224@gmail.com>
2024-05-23 23:46:02 +0900

    2024-05-23

---
+++ Counting/Counting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Counting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> m = new List<int>();
+
+            string[] na = Console.ReadLine().Trim().Split();
+            for (int i = 0; i < na.Length; i++)
+            {
+                m.Add(int.Parse(na[i]));
+            }
+            int v = int.Parse(Console.ReadLine());
+
+            m.RemoveAll((a) => a != v);
+
+            Console.WriteLine(m.Count);
+        }
+    }
+}
