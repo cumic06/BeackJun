11d2736ace9d930952af2d5b1c035204ec23946c
cumic <changhwan060224@gmail.com>
2024-05-25 20:38:21 +0900

    2024-05-25
    FibonacciDP

--- FibonacciDP/FibonacciDP/Program.cs
+++ FibonacciDP/FibonacciDP/Program.cs
@@ -1,30 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FibonacciDP
 {
     class Program
     {
+        static int DPCount = 0;
+
         static void Main()
         {
             int a = int.Parse(Console.ReadLine());
-            int fibonacciCount = 0;
-            for (int i = 0; i < a; i++)
-            {
-                fibonacciCount += Fibonacci(a);
-            }
-            Console.WriteLine(fibonacciCount);
+            FibonacciDP(a);
+            Console.WriteLine($"{Fibonacci(a)} {DPCount}");
         }
 
         static int Fibonacci(int a)
         {
-            return a;
+            if (a == 0)
+            {
+                return 0;
+            }
+            if (a == 1)
+            {
+                return 1;
+            }
+            return Fibonacci(a - 1) + Fibonacci(a - 2);
         }
-        //static int FibonacciDP(int a)
-        //{
 
-        //}
+        static int[] dp = new int[100];
+
+        static int FibonacciDP(int a)
+        {
+            if (a == 0)
+            {
+                return 0;
+            }
+            if (a == 1 || a == 2)
+            {
+                return 1;
+            }
+
+            if (dp[a] != 0)
+            {
+                return dp[a];
+            }
+
+            DPCount++;
+            return dp[a] = FibonacciDP(a - 1) + FibonacciDP(a - 2);
+        }
     }
 }
