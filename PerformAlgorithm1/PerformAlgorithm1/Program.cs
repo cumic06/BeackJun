26a09928112a5241642dc7747d2094da653c04f8
cumic <changhwan060224@gmail.com>
2024-06-05 23:47:16 +0900

    2024-06-05

---
+++ PerformAlgorithm1/PerformAlgorithm1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PerformAlgorithm1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(0);
+        }
+    }
+}
