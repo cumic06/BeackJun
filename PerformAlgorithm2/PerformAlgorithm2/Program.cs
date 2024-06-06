372cb641a7c781321e1dcb2be3c4cd41ddc7d954
cumic <changhwan060224@gmail.com>
2024-06-06 17:16:02 +0900

    2024-06-06

---
+++ PerformAlgorithm2/PerformAlgorithm2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PerformAlgorithm2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.WriteLine(1);
+        }
+    }
+}
