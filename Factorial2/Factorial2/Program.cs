ad88339d30cf591d168d7cae9fc4e3d77f8cb5f8
cumic <changhwan060224@gmail.com>
2024-06-09 21:26:54 +0900

    2024-06-09

---
+++ Factorial2/Factorial2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Factorial2
+{
+    class Program
+    {
+        static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            long result = 1;
+
+            result = Factorial(n);
+            Console.WriteLine(result);
+        }
+
+        static long Factorial(long n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                long result = n * Factorial(n - 1);
+                return result;
+            }
+        }
+    }
+}
---
+++ giantGreenTower/giantGreenTower/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace giantGreenTower
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int result = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                result *= 2;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
