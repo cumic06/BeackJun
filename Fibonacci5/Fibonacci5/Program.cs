c203c86bc2f94838f26a3b191067691c69c1393e
cumic <changhwan060224@gmail.com>
2024-06-10 23:23:42 +0900

    2024-06-10

---
+++ Fibonacci5/Fibonacci5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int fibonacciValue = 0;
+            fibonacciValue += Fibonacci(n);
+            Console.WriteLine(fibonacciValue);
+        }
+
+        static int Fibonacci(int a)
+        {
+            if (a == 0)
+            {
+                return 0;
+            }
+            if (a == 1)
+            {
+                return 1;
+            }
+            return Fibonacci(a - 1) + Fibonacci(a - 2);
+        }
+    }
+}
