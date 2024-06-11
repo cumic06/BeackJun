dedc862b402fd38c9b3e327ba18203a95fb5efbf
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-06-11 21:29:48 +0900

    1

--- Make1/Make1/Program.cs
+++ Make1/Make1/Program.cs
@@ -4,10 +4,41 @@ namespace Make1
 {
     class Program
     {
+        static int count = 0;
         static void Main()
         {
             int x = int.Parse(Console.ReadLine());
-            
+            int count = 0;
+
+            count += Make1(x);
+            Console.WriteLine(count);
+        }
+
+        static int Make1(int x)
+        {
+            if (x % 3 == 0)
+            {
+                x /= 3;
+            }
+            else if (x % 2 == 0)
+            {
+                x /= 2;
+                count++;
+            }
+            else
+            {
+                x--;
+                count++;
+            }
+
+            if (x == 1)
+            {
+                return count;
+            }
+            else
+            {
+                return Make1(x);
+            }
         }
     }
 }
