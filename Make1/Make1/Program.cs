c6f97f541a3ea37e21d040305de33576e93f342c
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-06-11 20:44:43 +0900

    2024-06-11
    1로 만들기 처음부터

--- Make1/Make1/Program.cs
+++ Make1/Make1/Program.cs
@@ -4,45 +4,10 @@ namespace Make1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int X = int.Parse(Console.ReadLine());
-            int count = 0;
-            if (X > 0)
-            {
-                while (X > 2)
-                {
-                    if (X % 3 == 0)
-                    {
-                        X /= 3;
-                        count++;
-                        Console.WriteLine("%3 " + X);
-                    }
-                    else if (X % 2 == 0)
-                    {
-                        X /= 2;
-                        count++;
-                        Console.WriteLine("%2 " + X);
-                    }
-                    else
-                    {
-                        Min(ref X, ref count);
-                    }
-                }
-                Min(ref X, ref count);
-                Console.WriteLine("result " + count);
-                Console.ReadKey();
-            }
-        }
-
-        static void Min(ref int X, ref int count)
-        {
-            if (X > 1)
-            {
-                X--;
-                count++;
-                Console.WriteLine("Min" + X);
-            }
+            int x = int.Parse(Console.ReadLine());
+            
         }
     }
 }
