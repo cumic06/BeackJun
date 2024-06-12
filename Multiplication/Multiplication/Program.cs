2cdf80e49c7e1b4711850cdabe7a20600458674e
cumic <changhwan060224@gmail.com>
2024-06-13 00:00:49 +0900

    2024-06-13

---
+++ Multiplication/Multiplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            string astr = Console.ReadLine();
+            string bstr = Console.ReadLine();
+            int a = int.Parse(astr);
+            int b = int.Parse(bstr);
+
+            Console.WriteLine(a * int.Parse(bstr[2].ToString()));
+            Console.WriteLine(a * int.Parse(bstr[1].ToString()));
+            Console.WriteLine(a * int.Parse(bstr[0].ToString()));
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
