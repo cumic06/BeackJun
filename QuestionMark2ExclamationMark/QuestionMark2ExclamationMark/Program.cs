01e941e4962887c8dc1faeed4c5292476a8ebde8
cumic <changhwan060224@gmail.com>
2024-06-13 22:57:40 +0900

    2024-06-13

---
+++ QuestionMark2ExclamationMark/QuestionMark2ExclamationMark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QuestionMark2ExclamationMark
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str + "??!");
+        }
+    }
+}
