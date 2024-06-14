f11265a74db10d1b298e65180a79dd874ac4b643
cumic <changhwan060224@gmail.com>
2024-06-14 23:28:47 +0900

    2024-06-14

---
+++ ThaiAge/ThaiAge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ThaiAge
+{
+    class Program
+    {
+        static void Main()
+        {
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(y - 543);
+        }
+    }
+}
