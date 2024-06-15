d892db498363a7a42fba19fb6b42102f09540d3c
cumic <changhwan060224@gmail.com>
2024-06-15 21:35:31 +0900

    2024-06-15

---
+++ BigintegerMultiplication/BigintegerMultiplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigintegerMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            BigInteger a = BigInteger.Parse(str[0]);
+            BigInteger b = BigInteger.Parse(str[1]);
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
