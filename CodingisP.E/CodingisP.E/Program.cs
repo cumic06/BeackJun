132cadfb1f929bbffab1ac809a2d30989f364b55
cumic <changhwan060224@gmail.com>
2024-06-16 23:58:54 +0900

    2024-06-16

---
+++ CodingisP.E/CodingisP.E/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace CodingisP.E
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(Console.ReadLine());
+            int t = n / 4;
+            for (int i = 0; i < t; i++)
+            {
+                sb.Append("long ");
+            }
+            sb.Append("int");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
