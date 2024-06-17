ff42d1e9cbf83b72ee17b862b548ca97921e5156
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-06-17 20:32:52 +0900

    2024-06-17

---
+++ A+B-8/A+B-8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace A_B_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+                sb.AppendLine($"Case #{i + 1}: {int.Parse(str[0])} + {int.Parse(str[1])} = {int.Parse(str[0]) + int.Parse(str[1])}");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
