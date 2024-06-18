4039e5a6650058d7b815c5db88fa395bd8fc1a41
cumic <changhwan060224@gmail.com>
2024-06-19 04:53:45 +0900

    2024-06-18

---
+++ Receipt/Receipt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Receipt
+{
+    class Program
+    {
+        static void Main()
+        {
+            int totalMoney = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+
+                for (int j = 0; j < int.Parse(str[1]); j++)
+                {
+                    totalMoney -= int.Parse(str[0]);
+                }
+            }
+            if (totalMoney == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
