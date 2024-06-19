80906efff3015acb17ea3c8c00da3a39de3275f0
cumic <changhwan060224@gmail.com>
2024-06-19 23:51:26 +0900

    2024-06-19

---
+++ const/const/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Const
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+
+            if (Rev(str[0]) > Rev(str[1]))
+            {
+                Console.WriteLine(Rev(str[0]));
+            }
+            else
+            {
+                Console.WriteLine(Rev(str[1]));
+            }
+        }
+
+        static int Rev(string value)
+        {
+            char[] abc = value.ToCharArray();
+
+            Array.Reverse(abc);
+
+
+            string resultString = new string(abc);
+            return int.Parse(resultString);
+        }
+    }
+}
