8933812eec66f41b4ccb909f4fd04ac67752643d
cumic <changhwan060224@gmail.com>
2024-06-20 23:20:21 +0900

    2024-06-20

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
