3e956816a466d4a85692e2990ca96bee14063e48
cumic <changhwan060224@gmail.com>
2024-06-21 23:58:41 +0900

    2024-06-21

---
+++ DariusNoHanta/DariusNoHanta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DariusNoHanta
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split('/');
+
+            if (int.Parse(str[1]) == 0)
+            {
+                Console.WriteLine("hasu");
+                return;
+            }
+
+            if (int.Parse(str[1]) <= int.Parse(str[0]) + int.Parse(str[2]))
+            {
+                Console.WriteLine("gosu");
+            }
+            else if (int.Parse(str[1]) > int.Parse(str[0]) + int.Parse(str[2]))
+            {
+                Console.WriteLine("hasu");
+            }
+        }
+    }
+}
