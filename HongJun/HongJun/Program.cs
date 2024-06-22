cde2d89862bdb9216fc7525768ddc0b31b1018a1
cumic <changhwan060224@gmail.com>
2024-06-22 23:43:54 +0900

    2024-06-22

---
+++ HongJun/HongJun/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HongJun
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+
+            Console.WriteLine(":fan::fan::fan:");
+            Console.WriteLine($":fan::{str}::fan:");
+            Console.WriteLine(":fan::fan::fan:");
+        }
+    }
+}
