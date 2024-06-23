d7e2798b132df3cc2f7ee73b7b8a1aaa56556a30
cumic <changhwan060224@gmail.com>
2024-06-23 20:32:03 +0900

    2024-06-23

---
+++ A+B-6/A+B-6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace A_B_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] str = Console.ReadLine().Split(',');
+                int a = int.Parse(str[0]) + int.Parse(str[1]);
+                sb.AppendLine(a.ToString());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
