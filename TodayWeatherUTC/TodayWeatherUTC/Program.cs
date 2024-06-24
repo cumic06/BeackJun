d9d337fb9c53770e46f11591d4af3a05e42e86cd
cumic <changhwan060224@gmail.com>
2024-06-24 23:30:32 +0900

    2024-06-24

---
+++ TodayWeatherUTC/TodayWeatherUTC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TodayWeatherUTC
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            string[] str = utcNow.ToString("yyyy-MM-dd").Split('-');
+            Console.WriteLine(str[0]);
+            Console.WriteLine(str[1]);
+            Console.WriteLine(str[2]);
+        }
+    }
+}
