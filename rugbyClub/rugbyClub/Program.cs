ab51dd83c6355f21e34acde3c543379339151510
cumic <changhwan060224@gmail.com>
2024-06-25 22:43:08 +0900

    2024-06-25

---
+++ rugbyClub/rugbyClub/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace rugbyClub
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str.Contains("#")) break;
+
+                string[] member = str.Split();
+
+                if (int.Parse(member[1]) <= 17)
+                {
+                    if (int.Parse(member[2]) >= 80)
+                    {
+                        sb.AppendLine($"{member[0]} Senior");
+                    }
+                    else
+                    {
+                        sb.AppendLine($"{member[0]} Junior");
+                    }
+                }
+                else
+                {
+                    sb.AppendLine($"{member[0]} Senior");
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
