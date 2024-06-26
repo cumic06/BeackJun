8fbaec6ab575da518d2f35baab5a3f2340f197d6
cumic <changhwan060224@gmail.com>
2024-06-26 23:43:19 +0900

    2024-06-26

---
+++ secretPassword/secretPassword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace secretPassword
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string str = Console.ReadLine();
+                if (str.Length >= 6 && str.Length <= 9)
+                {
+                    sb.AppendLine("yes");
+                }
+                else
+                {
+                    sb.AppendLine("no");
+                }
+            }
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
