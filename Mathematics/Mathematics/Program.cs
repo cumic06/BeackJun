917718750bb3410a8b80d34339777658ac1a70e6
cumic <changhwan060224@gmail.com>
2024-06-27 23:08:32 +0900

    2024-06-27

---
+++ Mathematics/Mathematics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mathematics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < t; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                result += a;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
