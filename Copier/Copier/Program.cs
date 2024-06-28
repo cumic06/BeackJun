2aac32402fc16c51d2c191935fe9ba7aea101e2a
cumic <changhwan060224@gmail.com>
2024-06-28 23:13:40 +0900

    2024-06-28

---
+++ Copier/Copier/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Copier
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                sb.AppendLine($"{n} {n}");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
