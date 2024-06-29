7120214632a8cd293d05b0256e1c4c520b3cb98d
cumic <changhwan060224@gmail.com>
2024-06-29 19:28:37 +0900

    2024-06-29

---
+++ Hour/Hour/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hour
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x * 24);
+        }
+    }
+}
