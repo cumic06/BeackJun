785af5fbf569fb55cf8007f2913aa63fbf606001
cumic <changhwan060224@gmail.com>
2024-06-30 21:41:29 +0900

    2024-06-30

---
+++ ThreeWayComparison/ThreeWayComparison/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ThreeWayComparison
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(1);
+            }
+            else if (a < b)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
