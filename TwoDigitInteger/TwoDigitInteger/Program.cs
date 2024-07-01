583dba5936bba2e8d0dfcd81533144a65b5e0828
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-07-01 20:16:20 +0900

    2024-07-01

---
+++ TwoDigitInteger/TwoDigitInteger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoDigitInteger
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
