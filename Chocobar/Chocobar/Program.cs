0a12db6ccec9c053a4eacaf8e40907162caea486
cumic <changhwan060224@gmail.com>
2024-07-03 23:17:27 +0900

    2024-07-03

---
+++ Chocobar/Chocobar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chocobar
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+
+            if (a * 100 >= b)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
