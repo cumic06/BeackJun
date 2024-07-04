10e8d42692bb576a41553e6f8a52b2d0b9b19414
cumic <changhwan060224@gmail.com>
2024-07-04 23:38:15 +0900

    2024-07-04

---
+++ PublicTransport/PublicTransport/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PublicTransport
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int n = int.Parse(str[0]);//subway
+            int a = int.Parse(str[1]);//bus
+            int b = int.Parse(str[2]);//subway
+
+            if (a < b)
+            {
+                Console.WriteLine("Bus");
+            }
+            else if (a > b)
+            {
+                Console.WriteLine("Subway");
+            }
+            else
+            {
+                Console.WriteLine("Anything");
+            }
+        }
+    }
+}
