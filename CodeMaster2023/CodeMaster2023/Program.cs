61f69f35db18ab7511c24e2130f359e9fd7e59aa
cumic <changhwan060224@gmail.com>
2024-07-05 20:27:16 +0900

    2024-07-05

---
+++ CodeMaster2023/CodeMaster2023/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeMaster2023
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            switch (str)
+            {
+                case "SONGDO":
+                    Console.WriteLine("HIGHSCHOOL");
+                    break;
+                case "CODE":
+                    Console.WriteLine("MASTER");
+                    break;
+                case "2023":
+                    Console.WriteLine("0611");
+                    break;
+                case "ALGORITHM":
+                    Console.WriteLine("CONTEST");
+                    break;
+            }
+        }
+    }
+}
