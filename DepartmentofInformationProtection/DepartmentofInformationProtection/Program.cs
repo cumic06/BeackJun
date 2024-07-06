9e9b230ec3082868c665dbfc8cdfbfea9efbd988
cumic <changhwan060224@gmail.com>
2024-07-06 23:58:30 +0000

    2024-07-07

---
+++ DepartmentofInformationProtection/DepartmentofInformationProtection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DepartmentofInformationProtection
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+
+            switch (str)
+            {
+                case "M":
+                    Console.WriteLine("MatKor");
+                    break;
+                case "W":
+                    Console.WriteLine("WiCys");
+                    break;
+                case "C":
+                    Console.WriteLine("CyKor");
+                    break;
+                case "A":
+                    Console.WriteLine("AlKor");
+                    break;
+                case "$":
+                    Console.WriteLine("$clear");
+                    break;
+            }
+        }
+    }
+}
