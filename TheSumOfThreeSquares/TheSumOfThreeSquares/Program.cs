5d3a2199754482af3c5f3565b080371e626cf887
cumic <changhwan060224@gmail.com>
2024-07-07 17:06:29 +0900

    2024-07-07

---
+++ TheSumOfThreeSquares/TheSumOfThreeSquares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheSumOfThreeSquares
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                a += i;
+                b += i;
+                c += i * i * i;
+            }
+            b *= b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
