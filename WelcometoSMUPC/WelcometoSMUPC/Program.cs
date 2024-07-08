f7361e6a2a964ba445035d7b057f4931c2ecbeb8
cumic <changhwan060224@gmail.com>
2024-07-09 00:00:18 +0900

    2024-07-08

---
+++ WelcometoSMUPC/WelcometoSMUPC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WelcometoSMUPC
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string str = "WelcomeToSMUPC";
+
+            int index = (n - 1) % 14;
+            Console.WriteLine(str[index]);
+        }
+    }
+}
