7841b72a20f4d8d9cec6438139f5aaca623f182a
cumic <changhwan060224@gmail.com>
2024-07-10 00:18:58 +0900

    2024-07-09

--- Triangle/Triangle/Program.cs
+++ Triangle/Triangle/Program.cs
@@ -7,8 +7,10 @@ namespace Triangle
         static void Main()
         {
             string[] str = Console.ReadLine().Split();
-            int a = int.Parse(str[0]);
-            int b = int.Parse(str[1]);
+
+            float a = float.Parse(str[0]);
+            float b = float.Parse(str[1]);
+
             float c = a * b * 0.5f;
             Console.WriteLine($"{c:0.0}");
         }
