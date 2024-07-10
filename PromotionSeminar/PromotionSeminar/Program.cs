521a5964950bfc42239bb75c1768c3532a82e9ef
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-07-10 18:38:43 +0900

    2024-07-10

---
+++ PromotionSeminar/PromotionSeminar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace PromotionSeminar
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string str = Console.ReadLine();
+
+                switch (str)
+                {
+                    case "Algorithm":
+                        sb.AppendLine("204");
+                        break;
+                    case "DataAnalysis":
+                        sb.AppendLine("207");
+                        break;
+                    case "ArtificialIntelligence":
+                        sb.AppendLine("302");
+                        break;
+                    case "CyberSecurity":
+                        sb.AppendLine("B101");
+                        break;
+                    case "Network":
+                        sb.AppendLine("303");
+                        break;
+                    case "Startup":
+                        sb.AppendLine("501");
+                        break;
+                    case "TestStrategy":
+                        sb.AppendLine("105");
+                        break;
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
