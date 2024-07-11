67b04aebd42f5d5595d5dba0cc6803c23fbdbb79
cumic <changhwan060224@gmail.com>
2024-07-11 23:08:24 +0900

    2024-07-11

---
+++ WhatColorIsThisStar/WhatColorIsThisStar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WhatColorIsThisStar
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n >= 380 && n < 425)
+            {
+                Console.WriteLine("Violet");
+            }
+            else if (n >= 425 && n < 450)
+            {
+                Console.WriteLine("Indigo");
+            }
+            else if (n >= 450 && n < 495)
+            {
+                Console.WriteLine("Blue");
+            }
+            else if (n >= 495 && n < 570)
+            {
+                Console.WriteLine("Green");
+            }
+            else if (n >= 570 && n < 590)
+            {
+                Console.WriteLine("Yellow");
+            }
+            else if (n >= 590 && n < 620)
+            {
+                Console.WriteLine("Orange");
+            }
+            else if (n >= 620 && n <= 780)
+            {
+                Console.WriteLine("Red");
+            }
+        }
+    }
+}
