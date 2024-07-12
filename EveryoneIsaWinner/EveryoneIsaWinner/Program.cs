da32dea253e88a1cd52e231dcac2079fe4dcc2c3
cumic <changhwan060224@gmail.com>
2024-07-12 22:33:17 +0900

    2024-07-12

---
+++ EveryoneIsaWinner/EveryoneIsaWinner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EveryoneIsaWinner
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+
+            if (a % b == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
