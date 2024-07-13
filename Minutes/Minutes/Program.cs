cdaba7a386a2216cb2836667a6063cc57bfbb4e2
cumic <changhwan060224@gmail.com>
2024-07-13 18:58:46 +0900

    2024-07-13

---
+++ Minutes/Minutes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Minutes
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * 60 + b);
+        }
+    }
+}
