bcd216d2061d365e0cc2b5ff27c5b5c513600805
cumic <changhwan060224@gmail.com>
2024-07-14 16:08:35 +0900

    2024-07-14

---
+++ AddingTrouble/AddingTrouble/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AddingTrouble
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+            int c = int.Parse(str[2]);
+
+            if (a + b == c)
+            {
+                Console.WriteLine("correct!");
+            }
+            else
+            {
+                Console.WriteLine("wrong!");
+            }
+        }
+    }
+}
