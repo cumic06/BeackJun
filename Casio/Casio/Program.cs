e33e7438f2a6b0af33f35b8847279012c5fa35c9
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-07-15 20:57:06 +0900

    2024-07-15

---
+++ Casio/Casio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Casio
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            long a = long.Parse(str[0]);
+            long b = long.Parse(str[1]);
+
+            if (a == b)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
