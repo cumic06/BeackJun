1fd75aa4335ff6b5fcfbde62fc9c17b5aabf5c47
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-07-16 19:59:45 +0900

    2024-07-16

---
+++ HongikUniversity/HongikUniversity/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HongikUniversity
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(n - 1946);
+        }
+    }
+}
