cee88e5dbeab5b01a00edc0de4bba75632cbca4f
cumic <changhwan060224@gmail.com>
2024-07-17 23:50:34 +0900

    2024-07-17

---
+++ Rats/Rats/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Rats
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+            int c = int.Parse(str[2]);
+
+            int d = (a + 1) * (b + 1) / (c + 1) - 1;
+
+            Console.WriteLine(d);
+        }
+    }
+}
