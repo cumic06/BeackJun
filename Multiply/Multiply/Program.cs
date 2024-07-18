35661dccc544b2a02b99721d3e561f9c02f9a9e3
bch6224 <92432064+cumic06@users.noreply.github.com>
2024-07-19 08:41:56 +0900

    2024-07-19

---
+++ Multiply/Multiply/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace Multiply
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            BigInteger b = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
