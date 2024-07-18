eb2f8a70c40958aa588bb67ffc648d9cf1c29228
cumic <changhwan060224@gmail.com>
2024-07-18 22:47:21 +0900

    2024-07-18

---
+++ Tax/Tax/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tax
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = n - (int)(n * 0.22f);
+            int b = n - (int)(n * 0.2f * 0.22f);
+
+            Console.WriteLine($"{a} {b}");
+        }
+    }
+}
