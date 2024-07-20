966ece6cd02ce988d1e21da2cc4270653b014ff5
cumic <changhwan060224@gmail.com>
2024-07-20 22:52:21 +0900

    2024-07-20

---
+++ StarJJick3/StarJJick3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace StarJJick3
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int a = int.Parse(Console.ReadLine());
+            for (int i = a - 1; i >= 0; i--)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    sb.Append("*");
+                }
+                sb.Append("\n");
+            }
+            Console.Write(sb.ToString());
+        }
+    }
+}
