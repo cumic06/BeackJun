1a257925da67fcfc22ee1e1fc079edcd7743f858
cumic <changhwan060224@gmail.com>
2024-07-21 13:54:30 +0900

    2024-07-21

---
+++ R2/R2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace R2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = Console.ReadLine().Split();
+            int r1 = int.Parse(str[0]);
+            int s = int.Parse(str[1]);
+
+            Console.WriteLine(s * 2 - r1);
+        }
+    }
+}
