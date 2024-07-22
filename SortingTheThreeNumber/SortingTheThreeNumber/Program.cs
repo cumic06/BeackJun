cf97792716dedc25f17fd885b3bcc219e0a78dcd
cumic <changhwan060224@gmail.com>
2024-07-22 22:51:16 +0900

    2024-07-22

---
+++ SortingTheThreeNumber/SortingTheThreeNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SortingTheThreeNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] str = Console.ReadLine().Split();
+
+            str = str.OrderBy(a => int.Parse(a)).ToArray();
+
+            sb.Append($"{int.Parse(str[0])} {int.Parse(str[1])} {int.Parse(str[2])}");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
