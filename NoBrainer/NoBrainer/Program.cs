671cafbb4c2a396fa6b57f203188cc5d5586f6cd
cumic <92432064+cumic06@users.noreply.github.com>
2024-07-23 15:36:39 +0900

    2024-07-23

---
+++ NoBrainer/NoBrainer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace NoBrainer
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int t = int.Parse(Console.ReadLine());
+
+            string[] str = new string[] { };
+
+            for (int i = 0; i < t; i++)
+            {
+                str = Console.ReadLine().Split();
+
+                int a = int.Parse(str[0]);
+                int b = int.Parse(str[1]);
+
+                if (a >= b)
+                {
+                    sb.AppendLine("MMM BRAINS");
+                }
+                else
+                {
+                    sb.AppendLine("NO BRAINS");
+                }
+            }
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
