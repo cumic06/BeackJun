d5199a6997cacc4bda2f54d3c30c8c89df6acf80
cumic <changhwan060224@gmail.com>
2024-07-24 23:46:53 +0900

    2024-07-24

---
+++ GnomeSequencing/GnomeSequencing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace GnomeSequencing
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+
+            sb.AppendLine("Gnomes:");
+            for (int i = 0; i < t; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+
+                if (int.Parse(str[0]) > int.Parse(str[1]))
+                {
+                    if (int.Parse(str[1]) > int.Parse(str[2]))
+                    {
+                        sb.AppendLine("Ordered");
+                    }
+                    else if (int.Parse(str[1]) < int.Parse(str[2]))
+                    {
+                        sb.AppendLine("Unordered");
+                    }
+                }
+                else if (int.Parse(str[0]) < int.Parse(str[1]))
+                {
+                    if (int.Parse(str[1]) < int.Parse(str[2]))
+                    {
+                        sb.AppendLine("Ordered");
+                    }
+                    else if (int.Parse(str[1]) > int.Parse(str[2]))
+                    {
+                        sb.AppendLine("Unordered");
+                    }
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
