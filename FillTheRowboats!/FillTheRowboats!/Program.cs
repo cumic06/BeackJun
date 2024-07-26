e36de192b7fce4771c0ee3a29ce519099756226d
cumic <changhwan060224@gmail.com>
2024-07-26 23:25:30 +0900

    2024-07-25 ~ 2024-07-26

---
+++ FillTheRowboats!/FillTheRowboats!/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace FillTheRowboats_
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    sb.Append($"{i} ");
+                    if (i == n) break;
+                    sb.Append("Go! ");
+                }
+                else
+                {
+                    sb.Append($"{i} ");
+                }
+            }
+            sb.Append("Go!");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
