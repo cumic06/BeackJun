c7f33794d0082fae679303f7ee89382778041c53
cumic <changhwan060224@gmail.com>
2024-07-28 23:20:10 +0900

    2024-07-28

---
+++ SpeedFinesAreNotFine!/SpeedFinesAreNotFine!/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SpeedFinesAreNotFine_
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (b > a)
+            {
+                if (b - a <= 20)
+                {
+                    Console.WriteLine($"You are speeding and your fine is ${100}.");
+                }
+                else if (b - a <= 30)
+                {
+                    Console.WriteLine($"You are speeding and your fine is ${270}.");
+                }
+                else if (b - a >= 31)
+                {
+                    Console.WriteLine($"You are speeding and your fine is ${500}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations, you are within the speed limit!");
+            }
+        }
+    }
+}
