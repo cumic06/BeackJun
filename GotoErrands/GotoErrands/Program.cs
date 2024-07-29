446aaf221e340eb4dde318a44885548b07d24fbc
cumic <92432064+cumic06@users.noreply.github.com>
2024-07-29 17:47:31 +0900

    2024-07-29

---
+++ GotoErrands/GotoErrands/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GotoErrands
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            int min = (a + b + c + d) / 60;
+            int sec = (a + b + c + d) % 60;
+            Console.WriteLine(min);
+            Console.WriteLine(sec);
+        }
+    }
+}
