1c5d4c6d5e5a0e1b544e279e901174a76fedbc96
cumic <92432064+cumic06@users.noreply.github.com>
2024-07-30 21:29:51 +0900

    2024-07-30

---
+++ SoundsFishy/SoundsFishy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundsFishy
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> lists = new List<int>();
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            lists.Add(a);
+            lists.Add(b);
+            lists.Add(c);
+            lists.Add(d);
+
+            if (lists.Equals(lists.OrderBy(e => e).ToList()))
+            {
+                Console.WriteLine("Fish Rising");
+            }
+            else if (lists.Equals(lists.OrderByDescending(e => e)))
+            {
+                Console.WriteLine("Fish Diving");
+            }
+            else
+            {
+                Console.WriteLine("No Fish");
+            }
+        }
+    }
+}
