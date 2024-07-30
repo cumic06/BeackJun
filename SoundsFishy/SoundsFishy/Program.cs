983a334eb4a79dd854eb3beac3fa9f0ef95443e7
cumic <92432064+cumic06@users.noreply.github.com>
2024-07-30 21:29:51 +0900

    2024-07-30

---
+++ SoundsFishy/SoundsFishy/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace SoundsFishy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                if (b > c)
+                {
+                    if (c > d)
+                    {
+                        Console.WriteLine("Fish Diving");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Fish");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Fish");
+                }
+            }
+            else if (a < b)
+            {
+                if (b < c)
+                {
+                    if (c < d)
+                    {
+                        Console.WriteLine("Fish Rising");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Fish");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Fish");
+                }
+            }
+            else
+            {
+                if (b == c)
+                {
+                    if (c == d)
+                    {
+                        Console.WriteLine("Fish At Constant Depth");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Fish");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Fish");
+                }
+            }
+        }
+    }
+}
