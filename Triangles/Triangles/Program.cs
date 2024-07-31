e7084f6e28d1925fffb1da9d93648df4b7def2c0
cumic <changhwan060224@gmail.com>
2024-07-31 22:49:43 +0900

    2024-07-31

---
+++ Triangles/Triangles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Triangles
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int a = 1;
+
+            while (a != 0)
+            {
+                a = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= a; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        sb.Append("*");
+                    }
+                    sb.AppendLine();
+                }
+            }
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
