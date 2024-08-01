32f2a50566f32d839309ac280253de1dfb0f33e5
cumic <92432064+cumic06@users.noreply.github.com>
2024-08-01 19:03:52 +0900

    2024-08-01

---
+++ BodyMassIndex/BodyMassIndex/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BodyMassIndex
+{
+    class Program
+    {
+        static void Main()
+        {
+            float weight = float.Parse(Console.ReadLine());
+            float height = float.Parse(Console.ReadLine());
+
+            float bmi = weight / (height * height);
+            if (bmi > 25)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else if (bmi > 18.5f && bmi < 25.0f)
+            {
+                Console.WriteLine("Normal weight");
+            }
+            else if (bmi < 18.5f)
+            {
+                Console.WriteLine("Underweight");
+            }
+        }
+    }
+}
