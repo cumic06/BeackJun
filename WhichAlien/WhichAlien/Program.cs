49d3ce059edf4b3cafadb0a9db729db904c22d66
cumic <changhwan060224@gmail.com>
2024-08-02 23:07:10 +0900

    2024-08-02

---
+++ WhichAlien/WhichAlien/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhichAlien
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a >= 3 && b <= 4)
+            {
+                Console.WriteLine("TroyMartian");
+            }
+            if (a <= 6 && b >= 2)
+            {
+                Console.WriteLine("VladSaturnian");
+            }
+            if (a <= 2 && b <= 3)
+            {
+                Console.WriteLine("GraemeMercurian");
+            }
+        }
+    }
+}
