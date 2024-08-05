fd79afae2e960acfeab0e214713989b6c5a38889
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-05 16:07:41 +0900

    2024-08-05

---
+++ ISpeakTXTMSG/ISpeakTXTMSG/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace ISpeakTXTMSG
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+
+            while (true)
+            {
+                str = Console.ReadLine();
+                switch (str)
+                {
+                    case "CU":
+                        sb.AppendLine("see you");
+                        break;
+                    case ":-)":
+                        sb.AppendLine("I’m happy");
+                        break;
+                    case ":-(":
+                        sb.AppendLine("I’m unhappy");
+                        break;
+                    case ";-)":
+                        sb.AppendLine("wink");
+                        break;
+                    case ":-P":
+                        sb.AppendLine("stick out my tongue");
+                        break;
+                    case "(~.~)":
+                        sb.AppendLine("sleepy");
+                        break;
+                    case "TA":
+                        sb.AppendLine("totally awesome");
+                        break;
+                    case "CCC":
+                        sb.AppendLine("Canadian Computing Competition");
+                        break;
+                    case "CUZ":
+                        sb.AppendLine("because");
+                        break;
+                    case "TY":
+                        sb.AppendLine("thank-you");
+                        break;
+                    case "YW":
+                        sb.AppendLine("you’re welcome");
+                        break;
+                    case "TTYL":
+                        sb.AppendLine("talk to you later");
+                        break;
+                    default:
+                        sb.AppendLine(str);
+                        break;
+                }
+                if (str == "TTYL") break;
+            }
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
