f6cd79eae2a67d5b88206e5af07929c5a33116dd
cumic <changhwan060224@gmail.com>
2024-08-07 23:14:38 +0900

    2024-08-07

---
+++ Dice/Dice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+                int a = int.Parse(str[0]);
+                int b = int.Parse(str[1]);
+
+                sb.AppendLine($"Case {i + 1}: {a + b}");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
