327be2a0fce3bc73f67b3255a2416f48f1e33de3
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-08 09:05:40 +0900

    2024-08-08

---
+++ HelloJudge/HelloJudge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace HelloJudge
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= t; i++)
+            {
+                sb.AppendLine($"Hello World, Judge {i}!");
+            }
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
