b3dc11ef75e47dc428981691c2d8a9fdd5062517
cumic <changhwan060224@gmail.com>
2024-08-10 18:39:32 +0900

    2024-08-10

---
+++ Quadrants/Quadrants/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Quadrants
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (true)
+            {
+                string[] strArray = Console.ReadLine().Split();
+
+                float a = float.Parse(strArray[0]);
+                float b = float.Parse(strArray[1]);
+
+                if (a > 0 && b > 0)
+                {
+                    sb.AppendLine("Q1");
+                }
+                else if (a < 0 && b > 0)
+                {
+                    sb.AppendLine("Q2");
+                }
+                else if (a < 0 && b < 0)
+                {
+                    sb.AppendLine("Q3");
+                }
+                else if (a > 0 && b < 0)
+                {
+                    sb.AppendLine("Q4");
+                }
+                else if (a == 0 || b == 0)
+                {
+                    sb.AppendLine("AXIS");
+                }
+                if (float.Parse(strArray[0]).Equals(0) && float.Parse(strArray[1]).Equals(0)) break;
+            }
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
