af16e89d25a70267facf251548060f4bb0ba4872
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-14 21:24:47 +0900

    2024-08-14

---
+++ SpecialDay/SpecialDay/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SpecialDay
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > 2)
+            {
+                Console.WriteLine("After");
+            }
+            else if (a == 2)
+            {
+                if (b > 18)
+                {
+                    Console.WriteLine("After");
+                }
+                else if (b == 18)
+                {
+                    Console.WriteLine("Special");
+                }
+                else
+                {
+                    Console.WriteLine("Before");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Before");
+            }
+        }
+    }
+}
