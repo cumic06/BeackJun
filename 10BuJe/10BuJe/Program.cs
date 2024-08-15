5f6e2627007d96bb8e7d338f35ec833b24a63f93
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-15 15:14:54 +0900

    2024-08-15

---
+++ 10BuJe/10BuJe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _10BuJe
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            string[] strArray = Console.ReadLine().Split();
+
+            int[] intArray = new int[5] { int.Parse(strArray[0]), int.Parse(strArray[1]), int.Parse(strArray[2]), int.Parse(strArray[3]), int.Parse(strArray[4]) };
+
+            int count = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i].Equals(a))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
