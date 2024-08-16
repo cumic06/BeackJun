e1204d4d0681915f676ea68f1a386c08eeaf38ef
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-16 20:43:16 +0900

    2024-08-16

---
+++ IdentifyingTea/IdentifyingTea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IdentifyingTea
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            string[] strArray = Console.ReadLine().Split();
+
+            int count = 0;
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (int.Parse(strArray[i]) == a)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
