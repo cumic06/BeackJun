25c6086e818e0d9d91aa392a1c4fce5dd5641245
cumic <changhwan060224@gmail.com>
2024-08-17 19:19:39 +0900

    2024-08-17

---
+++ SubjectSelect/SubjectSelect/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SubjectSelect
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] intArray = new int[4];
+            int[] intArray2 = new int[2];
+
+            for (int i = 0; i < 4; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                intArray[i] = a;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                intArray2[i] = a;
+            }
+            intArray = intArray.OrderByDescending(a => a).ToArray();
+            intArray2 = intArray2.OrderByDescending(a => a).ToArray();
+            Console.WriteLine(intArray[0] + intArray[1] + intArray[2] + intArray2[0]);
+
+        }
+    }
+}
