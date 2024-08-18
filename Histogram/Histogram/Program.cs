ab5ddfd333735f7de73465521a8febe10e8a6910
cumic <changhwan060224@gmail.com>
2024-08-18 18:03:01 +0900

    2024-08-18

---
+++ Histogram/Histogram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Histogram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int t = int.Parse(Console.ReadLine());
+
+            int a = 0;
+
+            for (int i = 0; i < t; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < a; j++)
+                {
+                    sb.Append("=");
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
