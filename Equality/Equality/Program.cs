1a502840e33187994b0a3bd515a7006862cf0938
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-19 08:46:41 +0900

    2024-08-19

---
+++ Equality/Equality/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Equality
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Trim().Split();
+
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            a = int.Parse(strArray[0]);
+            b = int.Parse(strArray[2]);
+            c = int.Parse(strArray[4]);
+
+            if ((a + b).Equals(c))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
