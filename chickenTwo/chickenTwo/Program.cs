5fdcd94c1fd2336775d470dda279e48933fc7646
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-21 16:57:52 +0900

    2024-08-21

---
+++ chickenTwo/chickenTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace chickenTwo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Split();
+            int c = int.Parse(Console.ReadLine());
+
+            int a = int.Parse(strArray[0]);
+            int b = int.Parse(strArray[1]);
+
+            if (a + b < c * 2)
+            {
+                Console.WriteLine(a + b);
+            }
+            else if (a + b >= c * 2)
+            {
+                Console.WriteLine(a + b - c * 2);
+            }
+        }
+    }
+}
