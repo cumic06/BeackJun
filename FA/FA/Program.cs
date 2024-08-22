9a71df7c4c0ae6700d2aea248541cf167e5d7f4d
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-22 20:28:06 +0900

    2024-08-22

---
+++ FA/FA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FA
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string x = Console.ReadLine();
+
+            int a = int.Parse(x[0].ToString());
+            int b = int.Parse(x[x.Length - 1].ToString());
+
+            string c = FA(a, b);
+
+            while (true)
+            {
+                if (c == FA(a, b))
+                {
+                    Console.WriteLine("FA");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("NFA");
+                    break;
+                }
+            }
+        }
+
+        static string FA(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+    }
+}
