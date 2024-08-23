da7e600d5ac8c435644477fbb4da805ce1ab6f8a
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-23 11:46:23 +0900

    2024-08-23

---
+++ JudgingMoose/JudgingMoose/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace JudgingMoose
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Split();
+
+            int a = int.Parse(strArray[0]);
+            int b = int.Parse(strArray[1]);
+
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("Not a moose");
+                return;
+            }
+
+            if (a == b)
+            {
+                Console.WriteLine($"Even {a + b}");
+            }
+            else
+            {
+                if (a > b)
+                {
+                    Console.WriteLine($"Odd {a * 2}");
+                }
+                else if (a < b)
+                {
+                    Console.WriteLine($"Odd {b * 2}");
+                }
+            }
+        }
+    }
+}
