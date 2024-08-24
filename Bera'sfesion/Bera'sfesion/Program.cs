17c941bbd87e291fae279edb026d7a6f13d42cfe
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-24 15:28:34 +0900

    2024-08-24

---
+++ Bera'sfesion/Bera'sfesion/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bera_sfesion
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * a - a);
+        }
+    }
+}
