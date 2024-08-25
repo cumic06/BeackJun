6384b511b00b963eab8af6c068aa95d003abe62a
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-25 22:15:24 +0900

    2024-08-25

---
+++ FastIO/FastIO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FastIO
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int numsOfTimes = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numsOfTimes; i++)
+            {
+                using (StringReader reader = new StringReader(Console.ReadLine()))
+                {
+                    string[] text = reader.ReadLine().Split(' ');
+                    int a = int.Parse(text[0]);
+                    int b = int.Parse(text[1]);
+                    sb.AppendLine((a + b).ToString());
+                }
+            }
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
