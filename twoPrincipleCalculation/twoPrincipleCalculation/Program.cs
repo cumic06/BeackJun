e9dc7a04ac4950a0511bf4a5d3580cc48852ba32
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-26 22:26:44 +0900

    2024-08-26

---
+++ twoPrincipleCalculation/twoPrincipleCalculation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace twoPrincipleCalculation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Split();
+
+            decimal[] intArray = new decimal[3];
+
+            intArray[0] = decimal.Parse(strArray[0]);
+            intArray[1] = decimal.Parse(strArray[1]);
+            intArray[2] = decimal.Parse(strArray[2]);
+
+            decimal a = intArray[0] * intArray[1] / intArray[2];
+            decimal b = intArray[0] / intArray[1] * intArray[2];
+
+            if (a > b)
+            {
+                Console.WriteLine($"{(int)a}");
+            }
+            else if (b > a)
+            {
+                Console.WriteLine($"{(int)b}");
+            }
+        }
+    }
+}
