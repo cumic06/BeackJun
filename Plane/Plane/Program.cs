c89f559d985b3019f2d27130dae09c5b4b0b2a03
cumic06 <92432064+cumic06@users.noreply.github.com>
2024-08-27 23:15:39 +0900

    2024-08-27

---
+++ Plane/Plane/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Plane
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Split();
+            int a = int.Parse(strArray[0]);
+            int b = int.Parse(strArray[1]);
+            int c = int.Parse(strArray[2]);
+            int d = int.Parse(strArray[3]);
+
+            Console.WriteLine((a * b) + (c * d));
+        }
+    }
+}
