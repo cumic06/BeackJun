574557d4f3764d0cf2fc7b23cc64e1ca657f9027
cumic <changhwan060224@gmail.com>
2024-08-30 22:08:48 +0900

    2024-08-30

---
+++ VacationHomWork/VacationHomWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VacationHomWork
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int l = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            float e = a / (float)c;
+            float f = b / (float)d;
+
+            if (Math.Ceiling(e) >= Math.Ceiling(f))
+            {
+                Console.WriteLine(l - Math.Ceiling(e));
+            }
+            else if (Math.Ceiling(e) <= Math.Ceiling(f))
+            {
+                Console.WriteLine(l - Math.Ceiling(f));
+            }
+        }
+    }
+}
