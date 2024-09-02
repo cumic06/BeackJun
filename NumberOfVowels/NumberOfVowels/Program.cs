c03d2b16023598a9a2b91be48662287660b78f0f
cumic06 <changhwan060224@gmail.com>
2024-09-02 16:53:43 +0900

    2024-09-02

---
+++ NumberOfVowels/NumberOfVowels/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumberOfVowels
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    count++;
+                }
+                else if (str[i] == 'e')
+                {
+                    count++;
+                }
+                else if (str[i] == 'i')
+                {
+                    count++;
+                }
+                else if (str[i] == 'o')
+                {
+                    count++;
+                }
+                else if (str[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
