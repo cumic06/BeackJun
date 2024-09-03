e3fe76ad7ba502a25ee9705db40f438a66e2afc6
cumic <changhwan060224@gmail.com>
2024-09-03 22:09:12 +0900

    2024-09-03

---
+++ AroundAboutPlayGround/AroundAboutPlayGround/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AroundAboutPlayGround
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            float pi = 3.141592f;
+            Console.WriteLine((2 * pi * b) + (2 * a));
+        }
+    }
+}
