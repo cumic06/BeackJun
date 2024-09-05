617105b00710f0f7c2f7c9deabf0079a5991ebaf
cumic06 <changhwan060224@gmail.com>
2024-09-05 09:08:27 +0900

    2024-09-05

---
+++ TimetoDecompress/TimetoDecompress/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace TimetoDecompress
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+            string[] strArray;
+            for (int i = 0; i < t; i++)
+            {
+                strArray = Console.ReadLine().Split();
+
+                for (int j = 0; j < int.Parse(strArray[0]); j++)
+                {
+                    sb.Append(strArray[1]);
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
