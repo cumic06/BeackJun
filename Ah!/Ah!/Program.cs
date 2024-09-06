c047a72a8dba68c7e78159a1202097803636d133
cumic06 <changhwan060224@gmail.com>
2024-09-06 14:28:44 +0900

    2024-09-06

---
+++ Ah!/Ah!/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ah_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string ah = Console.ReadLine();
+            string ah2 = Console.ReadLine();
+
+            if (ah.Length < ah2.Length)
+            {
+                Console.WriteLine("no");
+            }
+            else if (ah.Length >= ah2.Length)
+            {
+                Console.WriteLine("go");
+            }
+        }
+    }
+}
