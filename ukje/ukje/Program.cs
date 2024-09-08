e14e3c0af22ada07509751dae405529f2796279b
cumic <changhwan060224@gmail.com>
2024-09-08 19:33:22 +0900

    2024-09-08

---
+++ ukje/ukje/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ukje
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Split();
+            int a = int.Parse(strArray[0]);
+            int b = int.Parse(strArray[1]);
+
+            double m = (b - a) / (float)400;
+
+            double percent = 1 / (1 + Math.Pow(10, m));
+
+            Console.WriteLine(percent);
+        }
+    }
+}
