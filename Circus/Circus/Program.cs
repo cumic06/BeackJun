8a0a2edeb498678becb6b50d2953ea923fe96d23
cumic <changhwan060224@gmail.com>
2024-09-09 23:04:24 +0900

    2024-09-09

---
+++ Circus/Circus/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Circus
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            long a = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Sqrt(a * Math.PI) * 2);
+        }
+    }
+}
