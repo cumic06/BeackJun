35f28b8e115a0c7accb2755eda023e3ffcfc0bea
cumic06 <changhwan060224@gmail.com>
2024-09-10 20:28:52 +0900

    2024-09-10

---
+++ SunlinInternetHighSchoolSong/SunlinInternetHighSchoolSong/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace SunlinInternetHighSchoolSong
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int a = int.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+
+            for (int i = str.Length - 5; i < str.Length; i++)
+            {
+                sb.Append(str[i]);
+            }
+
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
