72ab55be06e83c42ef3b14f9525edd58793bf2b5
cumic06 <changhwan060224@gmail.com>
2024-09-11 17:59:49 +0900

    2024-09-11

---
+++ MrPresidentYouShouldGambleForFun/MrPresidentYouShouldGambleForFun/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MrPresidentYouShouldGambleForFun
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int total = 0;
+            while (true)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a == -1) break;
+                total += a;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
