c7ed023a4dea85bd80e04629d1f7b88d5a0308ed
cumic06 <changhwan060224@gmail.com>
2024-09-12 17:50:55 +0900

    2024-09-12

---
+++ WhatIsNewbieIsStandard/WhatIsNewbieIsStandard/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WhatIsNewbieIsStandard
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Split();
+
+            int n = int.Parse(strArray[0]);
+            int m = int.Parse(strArray[1]);
+
+            if (m <= n && m <= 2)
+            {
+                Console.WriteLine("NEWBIE!");
+            }
+            else if (m <= n && m >= 3)
+            {
+                Console.WriteLine("OLDBIE!");
+            }
+            else
+            {
+                Console.WriteLine("TLE!");
+            }
+        }
+    }
+}
