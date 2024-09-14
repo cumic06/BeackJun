36d5de297da7cd8617cca00ac7bca416fe52f56b
cumic <changhwan060224@gmail.com>
2024-09-14 23:08:32 +0900

    2024-09-14

---
+++ Exam/Exam/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Exam
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] strArray = Console.ReadLine().Split();
+            int a = int.Parse(strArray[0]);
+            int b = int.Parse(strArray[1]);
+            int c = int.Parse(strArray[2]);
+
+            int[] intArray = new int[3];
+            intArray[0] = a;
+            intArray[1] = b;
+            intArray[2] = c;
+
+            intArray = intArray.OrderByDescending(d => d).ToArray();
+            Console.WriteLine(intArray[0] + intArray[1]);
+        }
+    }
+}
